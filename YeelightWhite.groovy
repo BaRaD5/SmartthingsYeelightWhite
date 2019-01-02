af4960998d19b3c77012abb057bfe96f338fc4d2
holdestmade <holdestmade@users.noreply.github.com>
2019-01-02 16:33:31 +0000

    Update YeelightWhite.groovy

--- YeelightWhite.groovy
+++ YeelightWhite.groovy
@@ -40,7 +40,7 @@ metadata {
     valueTile("level", "device.level", inactiveLabel: false, decoration: "flat") {
         state "level", label: 'Level ${currentValue}%'
     }
-    controlTile("colorTempControl", "device.colorTemperature", "slider", height: 1, width: 1, inactiveLabel: false, range:"(1700..6500)") {
+    controlTile("colorTempControl", "device.colorTemperature", "slider", height: 1, width: 1, inactiveLabel: false, range:"(2700..6500)") {
         state "colorTemperature", action:"setColorTemperature"
     }
     standardTile("coolWhite", "device.colorName", height: 1, width: 1, decoration: "flat", inactiveLabel: false, canChangeIcon: false) {
